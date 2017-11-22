6778061a44be1b17ef8b1193f2e9a8aa2a8c39ef
Larsenv <larsen@weborican.com>
2017-11-21 19:24:02 -0600

    Spectra;

--- spotlight.rb
+++ spotlight.rb
@@ -96,3 +96,4 @@ puts "Larsenv: " + random_larsenv_response
 puts spotlight_name + ": " + random_spotlight_response_2
 puts "Larsenv: " + "OK"
 puts spotlight_name + ": " + "/afk " + random_spotlight_afk_message
+puts "Spectra: ✅ " + spotlight_name + " has gone AFK"
