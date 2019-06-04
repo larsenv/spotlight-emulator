adb6e7bfdf7d9389fa903927cf2020fd3c34cca5
Larsen Vallecillo <larsen@Weborican.com>
2019-06-04 17:19:56 -0500

    Finally another Snoot update

---
+++ spotlight2.rb
@@ -0,0 +1,7 @@
+spotlight_name = ["Spotlight", "Spotlight", "Spotlight", "Spotlight", "Spotlight", "Snoot 🐾", "Snoot 🐾", "Snoot 🐾", "Snoot 🐾", "Snoot 🐾", "awesomebing1"].sample
+print "What would you like to say to " + spotlight_name + "? "
+message = gets
+
+puts ""
+print spotlight_name + ": "
+puts ["ayy", "a", "e", "ah", "ah alright", "alright", "lol", "lolol", "xD", "xd", "LMFAO", "Lmao", "lmao", "enjoy"].sample
